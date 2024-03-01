34701761cf221fb14d146e4cd13d6739a84671e2
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-03-01 21:08:40 +0600

    Permissions and Authenticated Classes

--- core/settings.py
+++ core/settings.py
@@ -135,7 +135,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.AllowAny',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 from datetime import timedelta
 
 
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated,AllowAny
 # Create your views here.
 
 
@@ -38,6 +39,7 @@ def registraionAPI(request):
 
 
 @api_view(['GET','POST'])
+# @permission_classes([IsAuthenticated,])
 def firstapi(request):
     if request.method == 'POST':
         name = request.POST['name']
