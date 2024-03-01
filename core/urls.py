8335f0e419452fc9e3e7ebe2ec86ed79a1b5a625
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-03-01 19:44:33 +0600

    Simple JWT based Login API and Customization

--- core/urls.py
+++ core/urls.py
@@ -18,11 +18,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
+from rest_framework.authtoken.views import obtain_auth_token
+from . views import MyTokenObtainPairView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/api_login/',obtain_auth_token),
+    # path('api/api_login/',obtain_auth_token),
+    path('api/api_login/token/',MyTokenObtainPairView.as_view()),
+    # path('api/api_login/token/',TokenObtainPairView.as_view()),
+    path('api/api_login/token/refresh/',TokenRefreshView.as_view()),
     path('api-auth/', include('rest_framework.urls')),
     path('api/firstapp/',include('firstapp.urls')),
 
